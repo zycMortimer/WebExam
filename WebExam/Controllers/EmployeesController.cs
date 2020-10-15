f572264929f1c235d5f756ff49b6c7df47225946
YI-ZHANG,CHEN <wl02075125@gmail.com>
2020-10-15 22:37:22 +0800

    2.加入Index.cshtml顯示Employee

---
+++ WebExam/Controllers/EmployeesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebExam.Models;
+
+namespace WebExam.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private CmsContext db = new CmsContext();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            //從資料庫讀取資料，建立model
+            var emps = db.Employees.ToList();
+
+            return View(emps);
+        }
+    }
+}
